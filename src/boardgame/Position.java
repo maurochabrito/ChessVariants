553749466175a174a34b48e0b9a571640b790cd9
maurochabrito <mauriciorochasic@gmail.com>
2022-11-16 05:13:19 -0300

    First Class: implementing Position Class

---
+++ src/boardgame/Position.java
@@ -0,0 +1,29 @@
+package boardgame;
+
+public class Position {
+	int row;
+	int collumn;
+	
+	public Position(int row, int collumn) {
+		this.row = row;
+		this.collumn = collumn;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getCollumn() {
+		return collumn;
+	}
+	public void setCollumn(int collumn) {
+		this.collumn = collumn;
+	}
+
+	@Override
+	public String toString() {
+		return row + ", " + collumn;
+	}
+}
