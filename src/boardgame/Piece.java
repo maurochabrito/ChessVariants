690f36d1c29ee98606f175e61d385f423530b6cc
maurochabrito <mauriciorochasic@gmail.com>
2022-11-17 02:00:27 -0300

    A small improvement at 'isTherePossibleMoves' on Piece class, considering a non-square board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -21,7 +21,7 @@ public abstract class Piece {
 	public boolean isThereAnyPossibleMove() {
 		boolean[][] mat = possibleMoves();
 		for (int i=0; i<mat.length; i++) {
-			for (int j=0; j<mat.length; j++) {
+			for (int j=0; j<mat[0].length; j++) {
 				if (mat[i][j]) {
 					return true;
 				}
