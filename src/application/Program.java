9d5098f2ea6c198b1901807311ee299542906d0d
maurochabrito <mauriciorochasic@gmail.com>
2022-11-17 21:38:45 -0300

    Handling captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,8 @@
 package application;
 
-import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -14,11 +15,12 @@ public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				System.out.println();
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
@@ -29,6 +31,9 @@ public class Program {
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 			}
 			catch(InputMismatchException e) {
 				System.out.println(e.getMessage());
--- src/application/ProgramTesting.java
+++ src/application/ProgramTesting.java
@@ -12,35 +12,5 @@ import chess.ChessPosition;
 public class ProgramTesting {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		ChessMatch chessMatch = new ChessMatch();
-			try {
-				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces());
-				System.out.println();
-				System.out.print("Source: ");
-				ChessPosition source = UI.readChessPosition(sc);
-				System.out.println();
-				System.out.print("Target: ");
-				ChessPosition target = UI.readChessPosition(sc);
-				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				boolean[][] matrix = (chessMatch.getPieces()[7][0].possibleMoves());
-				for (int i = 0; i<8; i++) {
-					for (int j = 0; j<8; j++) {
-						System.out.print(matrix[i][j]);
-					}
-					System.out.println();
-				}
-				System.out.println(chessMatch.getPieces()[7][0].possibleMove(new Position(7,4)));
-			}
-			catch(InputMismatchException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
-			catch(ChessException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
 	}
-
 }
