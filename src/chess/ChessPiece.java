71c85cda6e633e13cffa130ca7bfa12ec852b7cf
maurochabrito <mauriciorochasic@gmail.com>
2022-11-18 02:06:52 -0300

    Implementing ChessPiece movecount

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,7 +6,8 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 	private Color color;
-	
+	private int moveCount;
+
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
@@ -15,7 +16,17 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}
+	public int getMoveCount() {
+		return moveCount;
+	}
 
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
