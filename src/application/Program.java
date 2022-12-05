285be13f963419537f2ec8c7e0cc640e69c229af
maurochabrito <mauriciorochasic@gmail.com>
2022-12-05 02:31:44 -0300

    Implemented a Game Menu; Finished Capablanca Gothic Chess.

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,47 +15,59 @@ public class Program {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		ChessMatch chessMatch = new CapablancaChessMatch("Gothic");
-		List<ChessPiece> captured = new ArrayList<>();
+		System.out.println("Choose a Chess Variant to play (type the number):");
+		System.out.printf("%nAvaliable:%n(1) Capablanca's Chess%n%n>> ");
+		String variant = sc.nextLine();
+		switch(variant){
+			case "1":
+				System.out.println("Choose a Capablanca Chess initial setup (type the name):");
+				System.out.printf("%nAvaliable:%nGothic%n%n>> ");
+				String initialSetup = sc.nextLine();
+				ChessMatch chessMatch = new CapablancaChessMatch(initialSetup);
+				List<ChessPiece> captured = new ArrayList<>();
 
-		while (!chessMatch.getCheckMate()) {
-			try {
+				while (!chessMatch.getCheckMate()) {
+					try {
+						UI.clearScreen();
+						UI.printMatch(chessMatch, captured);
+						System.out.println();
+						System.out.print("Source: ");
+						ChessPosition source = UI.readChessPosition(sc);
+						boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+						UI.clearScreen();
+						UI.printBoard(chessMatch.getPieces(), possibleMoves);
+						System.out.println();
+						System.out.print("Target: ");
+						ChessPosition target = UI.readChessPosition(sc);
+						ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+						if (capturedPiece != null) {
+							captured.add(capturedPiece);
+						}
+						if (chessMatch.getPromoted() != null) {
+							System.out.print("Enter piece for promotion (B/N/R/A/C/Q): ");
+							String type = sc.nextLine().toUpperCase();
+							while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("A") && !type.equals("C") && !type.equals("Q")) {
+								System.out.print("Invalid value! Enter piece for promotion (B/N/R/A/C/Q): ");
+								type = sc.nextLine().toUpperCase();
+							}
+							chessMatch.replacePromotedPiece(type);
+						}
+					}
+					catch(InputMismatchException e) {
+						System.out.println(e.getMessage());
+						sc.nextLine();
+					}
+					catch(ChessException e) {
+						System.out.println(e.getMessage());
+						sc.nextLine();
+					}
+				}
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
-				System.out.println();
-				System.out.print("Source: ");
-				ChessPosition source = UI.readChessPosition(sc);
-				boolean[][] possibleMoves = chessMatch.possibleMoves(source);
-				UI.clearScreen();
-				UI.printBoard(chessMatch.getPieces(), possibleMoves);
-				System.out.println();
-				System.out.print("Target: ");
-				ChessPosition target = UI.readChessPosition(sc);
-				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				if (capturedPiece != null) {
-					captured.add(capturedPiece);
-				}
-				if (chessMatch.getPromoted() != null) {
-					System.out.print("Enter piece for promotion (B/N/R/A/C/Q): ");
-					String type = sc.nextLine().toUpperCase();
-					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("A") && !type.equals("C") && !type.equals("Q")) {
-						System.out.print("Invalid value! Enter piece for promotion (B/N/R/A/C/Q): ");
-						type = sc.nextLine().toUpperCase();
-					}
-					chessMatch.replacePromotedPiece(type);
-				}
-			}
-			catch(InputMismatchException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
-			catch(ChessException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
+		    break;
+		    default:
+		    break;
 		}
-		UI.clearScreen();
-		UI.printMatch(chessMatch, captured);
 	}
 
 }
